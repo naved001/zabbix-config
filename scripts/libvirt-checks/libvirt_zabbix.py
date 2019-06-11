d871b3c48f6fce71a2ef5fb280823c2ea6336646
Naved Ansari <naved001@gmail.com>
2019-06-11 15:45:02 -0400

    Just flesh out how to do things.

--- scripts/libvirt-checks/libvirt_zabbix.py
+++ scripts/libvirt-checks/libvirt_zabbix.py
@@ -1,19 +1,34 @@
 #! /usr/bin/python
 
 """Perform libvirt checks and package the data for zabbix server."""
+
+
 import sys
 import json
 from libvirt_checks import LibvirtConnection
+from pyzabbix import ZabbixMetric, ZabbixSender
+
 
 def main():
     """main I guess"""
-        libvirtconnection = LibvirtConnection()
+    zabbix_server = "zabbix.massopen.cloud"
+    zabbix_host = "hostname-in-zabbix"
+
+    libvirtconnection = LibvirtConnection()
+    zbx = ZabbixSender(zabbix_server)
+
+
+    # Main should discover all domains first, and then get all statistics for all domains,
+    # create a list of all metrics [(zabbix_host, key, value)...] and then send them.
     if len(sys.argv) >= 2:
         ret = getattr(libvirtconnection, sys.argv[1])(*sys.argv[2:])
         sys.stdout.write(ret)
     else:
         ret = libvirtconnection.discover_domains()
         sys.stdout.write(ret)
+
+def zabbix_key():
+    """Return the zabbix key for an item"""
 
 
 if __name__ == "__main__":
